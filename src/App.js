01eadd5a3bedd4f4699e76882c0a8b8e8e3be266
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-11 14:22:46 +0100

    Test rendering a header on our app red-green test

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        
+      <div data-test="component-app">
+        <h1>App</h1>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,24 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter()})
 
+test('renders without error', () =>{
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']")
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders increment button', () => {
+  
+});
+
+test('renders counter display', () => {
+
+});
+
+test('counter starts at 0', () => {
+  
+});
+
+test('renders clicking button increments counter display', () => {
+
+});
